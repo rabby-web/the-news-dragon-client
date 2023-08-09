c32a5049249a2b5433a1d72e2e9c9eff6cf9027e
rabby-web <rabby.webdeveloper@gmail.com>
2023-08-10 00:06:11 +0600

    slice data icon add

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -1,13 +1,25 @@
-
+import { FaRegBookmark, FaRegShareFromSquare } from "react-icons/fa6";
+import moment from 'moment/moment';
+import { Image } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 
 const NewsCard = ( {news} ) => {
-    const { _id, title, details, image_url } = news;
+    const { _id, title, details, image_url, author } = news;
     return (
         <div>
-            <Card className="">
-                <Card.Header></Card.Header>
+            <Card className="mb-4">
+                <Card.Header className="d-flex align-items-center">
+                  <Image style={{height: '44px'}} src={author?.img} roundedCircle></Image>
+                  <div className='ps-3 flex-grow-1 lh-1'>
+                    <p>{author?.name}</p>
+                    <p><small>{moment(author.published_date).format('yyyy-MM-D')} {}</small></p>
+                  </div>
+                  <div>
+                    <FaRegBookmark className="me-2"></FaRegBookmark>
+                    <FaRegShareFromSquare></FaRegShareFromSquare>
+                  </div>
+                </Card.Header>
                 <Card.Body>
                   <Card.Title>{title}</Card.Title>
                   <Card.Text>
@@ -26,3 +38,5 @@ const NewsCard = ( {news} ) => {
 
 export default NewsCard;
 {/* <Card.Img variant="top" src={image_url} /> */}
+
+
