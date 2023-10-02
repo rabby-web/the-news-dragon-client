e20d3073827704267075c106872fa5254707b4c0
rabby-web <rabby.webdeveloper@gmail.com>
2023-10-02 23:33:05 +0600

    props type add

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -1,57 +1,78 @@
-import { FaEye, FaRegBookmark, FaRegShareFromSquare, FaRegStar, FaStar } from "react-icons/fa6";
-import moment from 'moment/moment';
-import { Image } from 'react-bootstrap';
-import Card from 'react-bootstrap/Card';
-import { Link } from 'react-router-dom';
+import {
+  FaEye,
+  FaRegBookmark,
+  FaRegShareFromSquare,
+  FaRegStar,
+  FaStar,
+} from "react-icons/fa6";
+import moment from "moment/moment";
+import { Image } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
 import Rating from "react-rating";
+import PropTypes from "prop-types";
 
-const NewsCard = ( {news} ) => {
-    const { _id, title, details, image_url, author, rating, total_view } = news;
-    return (
-        <div>
-            <Card className="mb-4">
-                <Card.Header className="d-flex align-items-center">
-                  <Image style={{height: '44px'}} src={author?.img} roundedCircle></Image>
-                  <div className='ps-3 flex-grow-1 lh-1'>
-                    <p>{author?.name}</p>
-                    <p><small>{moment(author.published_date).format('yyyy-MM-D')} {}</small></p>
-                  </div>
-                  <div>
-                    <FaRegBookmark className="me-2"></FaRegBookmark>
-                    <FaRegShareFromSquare></FaRegShareFromSquare>
-                  </div>
-                </Card.Header>
-                <Card.Body>
-                  <Card.Title>{title}</Card.Title>
-                  <Card.Text>
-                  
-                    {details.length < 20 ? <>{details}</>:
-                    <>{details.slice(0, 250)}...<Link to={`/news/${_id}`}>Reade More</Link></>
-                    
-                    }
-                  </Card.Text>
-                </Card.Body>
-                <Card.Footer className="text-muted d-flex">
-                  <div className="flex-grow-1">
-                  <Rating
-                    placeholderRating={rating.number}
-                    readonly
-                    emptySymbol={<FaRegStar></FaRegStar>}
-                    placeholderSymbol={<FaStar></FaStar>}
-                    fullSymbol={<FaStar></FaStar>}
-                  />
-                    <span> {rating.number}</span>
-                  </div>
-                  <div>
-                  <FaEye></FaEye> {total_view}
-                  </div>
-                </Card.Footer>
-            </Card>
-        </div>
-    );
+const NewsCard = ({ news }) => {
+  const { _id, title, details, author, rating, total_view } = news;
+  return (
+    <div>
+      <Card className="mb-4">
+        <Card.Header className="d-flex align-items-center">
+          <Image
+            style={{ height: "44px" }}
+            src={author?.img}
+            roundedCircle
+          ></Image>
+          <div className="ps-3 flex-grow-1 lh-1">
+            <p>{author?.name}</p>
+            <p>
+              <small>
+                {moment(author.published_date).format("yyyy-MM-D")} {}
+              </small>
+            </p>
+          </div>
+          <div>
+            <FaRegBookmark className="me-2"></FaRegBookmark>
+            <FaRegShareFromSquare></FaRegShareFromSquare>
+          </div>
+        </Card.Header>
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>
+            {details.length < 20 ? (
+              <>{details}</>
+            ) : (
+              <>
+                {details.slice(0, 250)}...
+                <Link to={`/news/${_id}`}>Reade More</Link>
+              </>
+            )}
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer className="text-muted d-flex">
+          <div className="flex-grow-1">
+            <Rating
+              placeholderRating={rating.number}
+              readonly
+              emptySymbol={<FaRegStar></FaRegStar>}
+              placeholderSymbol={<FaStar></FaStar>}
+              fullSymbol={<FaStar></FaStar>}
+            />
+            <span> {rating.number}</span>
+          </div>
+          <div>
+            <FaEye></FaEye> {total_view}
+          </div>
+        </Card.Footer>
+      </Card>
+    </div>
+  );
 };
 
+NewsCard.propTypes = {
+  news: PropTypes.object,
+};
 export default NewsCard;
-{/* <Card.Img variant="top" src={image_url} /> */}
-
-
+{
+  /* <Card.Img variant="top" src={image_url} /> */
+}
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -3,6 +3,7 @@ import {
   getAuth,
   signInWithEmailAndPassword,
 } from "firebase/auth";
+import PropTypes from "prop-types";
 import app from "../firebase/firebase.config";
 import { createContext } from "react";
 
@@ -26,6 +27,10 @@ const AuthProvider = ({ children }) => {
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node,
 };
 
 export default AuthProvider;
