15b8055fa1af690fd330b70729fd8e5513ffe858
rabby-web <rabby.webdeveloper@gmail.com>
2023-10-03 01:00:54 +0600

    loading add

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -13,19 +13,24 @@ const auth = getAuth(app);
 export const AuthContext = createContext(null);
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const createUser = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
   const signIn = (email, password) => {
+    setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
   const logOut = () => {
+    setLoading(true);
     return signOut(auth);
   };
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (loggedUser) => {
       setUser(loggedUser);
+      setLoading(false);
     });
     return () => {
       unsubscribe;
@@ -36,6 +41,7 @@ const AuthProvider = ({ children }) => {
     createUser,
     signIn,
     logOut,
+    loading,
   };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -4,9 +4,11 @@ import PropTypes from "prop-types";
 import { Navigate, useLocation } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
-  const { user } = useContext(AuthContext);
+  const { user, loading } = useContext(AuthContext);
   const location = useLocation();
-
+  if (loading) {
+    return <span className="loading loading-spinner loading-lg mx-auto"></span>;
+  }
   if (user) {
     return children;
   }
