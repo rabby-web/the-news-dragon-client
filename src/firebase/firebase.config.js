af78a474a94226c46561d781c5dda0505116b4ba
rabby-web <rabby.webdeveloper@gmail.com>
2023-10-03 01:32:12 +0600

    hide firebase confige

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,15 +2,16 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+console.log("environment variable", import.meta.env.VITE_PASS);
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCMs0JEm9m0osm2apl6RyKctJ0XM9PBZDk",
-  authDomain: "the-news-dragon-a4f00.firebaseapp.com",
-  projectId: "the-news-dragon-a4f00",
-  storageBucket: "the-news-dragon-a4f00.appspot.com",
-  messagingSenderId: "678580648109",
-  appId: "1:678580648109:web:67e289e8c0916d1e9c6e24"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
